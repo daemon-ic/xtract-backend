28b89ca03dfbadef4fd021a6a8a0edc325ce7d46
Alvin Sewram <alvinsewram@Alvins-MacBook-Pro.local>
2021-10-04 12:09:56 -0400

    added cors options

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
+
 const cors = require("cors");
 
 require("dotenv").config();
@@ -17,6 +18,7 @@ mongoose
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
+app.options("*", cors());
 
 app.use("/user", require("./routes/userRoutes"));
 app.use("/pptr", require("./routes/pptrRoutes"));
